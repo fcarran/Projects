a3e700622a235abd119f07f14b7d25105556137a
Fernando Carranza <fernandocarranza@Fernando-PersonalMacbook.local>
2018-08-05 20:05:29 -0700

    testing js file loads as extension in chrome, it does

--- browserTab/wellHello.js
+++ browserTab/wellHello.js
@@ -1,1 +1,17 @@
-//alert("Hi there");
+/*
+---Example DOM manipulation, changes body color from white to blue every 1 second
+var body = document.querySelector("body");
+var isBlue = false; 
+
+setInterval(function(){
+    if(isBlue){
+        body.style.background = "white";
+    } else {
+        body.style.background = "#3498db";
+    }
+    isBlue = !isBlue;
+}, 1000);
+*/
+
+var weather = document.querySelector("#weather");
+weather.style.color = "blue";
