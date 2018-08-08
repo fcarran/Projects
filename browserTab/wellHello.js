2661380c2aa41dd53036bc3d81adb44705b92584
Fernando Carranza <fernandocarranza@Fernando-PersonalMacbook.local>
2018-08-07 22:57:06 -0700

    add in two new css classes to toggle based on time of day

--- browserTab/wellHello.js
+++ browserTab/wellHello.js
@@ -1,7 +1,7 @@
 /*
 ---Example DOM manipulation, changes body color from white to blue every 1 second
 var body = document.querySelector("body");
-var isBlue = false; 
+var isBlue = false;
 
 setInterval(function(){
     if(isBlue){
@@ -13,10 +13,23 @@ setInterval(function(){
 }, 1000);
 */
 
+var timeOfDay = document.querySelector("body");
+
+var timeInMs = new Date().getHours()
+// console.log(timeInMs);
+//if time is less than noon (12)
+if(timeInMs < 12){
+    timeOfDay.classList.toggle("day"); //toggle morning css
+}
+//if time is more than 12
+if(timeInMs > 12){
+    timeOfDay.classList.toggle("night"); //toggle day css
+}
+
 var weather = document.querySelector(".weather");
 // San Jose api call https://api.darksky.net/forecast/30140b1b2d59c74c554f1d3b9a88f167/37.3382,121.8863
 
-var click = document.querySelector("button");
+/*var click = document.querySelector("button");
 var bod = document.querySelector("body");
 var isColor = false;
 
@@ -29,5 +42,9 @@ click.addEventListener("click", function(){
         isColor = true;
     }
 });
+*/
+
+
+
 
 
