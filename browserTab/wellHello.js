5704ba75401a816179b85b8b23c5066d23676cff
Fernando Carranza <fernandocarranza@Fernando-PersonalMacbook.local>
2018-08-14 22:50:03 -0700

    implemented clock + beginning of making weather api call

--- browserTab/wellHello.js
+++ browserTab/wellHello.js
@@ -1,49 +1,65 @@
-/*
----Example DOM manipulation, changes body color from white to blue every 1 second
-var body = document.querySelector("body");
-var isBlue = false;
+//body alter
+const timeOfDay = document.querySelector("body");
 
-setInterval(function(){
-    if(isBlue){
-        body.style.background = "white";
-    } else {
-        body.style.background = "#3498db";
+function isItDark(){
+    var timeInMs = new Date().getHours()
+    // console.log(timeInMs);
+    //if time is less than noon (12)
+    if(timeInMs < 12){
+        timeOfDay.classList.toggle("day"); //toggle morning css
     }
-    isBlue = !isBlue;
-}, 1000);
-*/
-
-var timeOfDay = document.querySelector("body");
-
-var timeInMs = new Date().getHours()
-// console.log(timeInMs);
-//if time is less than noon (12)
-if(timeInMs < 12){
-    timeOfDay.classList.toggle("day"); //toggle morning css
-}
-//if time is more than 12
-if(timeInMs > 12){
-    timeOfDay.classList.toggle("night"); //toggle day css
+    //if time is more than 12
+    if(timeInMs > 12){
+        timeOfDay.classList.toggle("night"); //toggle day css
+    }
 }
 
+isItDark();
+
+//weather alter
 var weather = document.querySelector(".weather");
-// San Jose api call https://api.darksky.net/forecast/30140b1b2d59c74c554f1d3b9a88f167/37.3382,121.8863
+// San Jose api call https://api.darksky.net/forecast/30140b1b2d59c74c554f1d3b9a88f167/37.3382,121.8863/exclude=
 
-/*var click = document.querySelector("button");
-var bod = document.querySelector("body");
-var isColor = false;
-
-click.addEventListener("click", function(){
-    if(isColor){
-        bod.style.backgroundColor = "white";
-        isColor = false;
-    } else {
-        bod.style.backgroundColor = "orange";
-        isColor = true;
+var getWeather = function() {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            showWeather(lat, long)
+        })
     }
-});
-*/
+    else {
+        window.alert("Unable to find location");
+    }
+}
 
+
+//time alter
+//TODO setup if statement for day/night clock color
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; //adjust the number of degrees that secondsDegrees specifies
+    //console.log(seconds);
+        
+    //set min hand interval
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`; //adjust the number of degrees that minutesDegrees specifies
+    //console.log(minutes);
+    //set hour hand interval
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`; //adjust the number of degrees that hourDegrees specifies
+    console.log(hours);
+}
+setInterval(setDate, 1000);
 
 
 
